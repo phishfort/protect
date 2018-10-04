7c263dcdec4b19f4197bbb23642707cc54581b84
kyle <syncikin@gmail.com>
2018-10-04 14:56:09 +0200

    bug fixes and cleanup

--- js/background.js
+++ js/background.js
@@ -2,14 +2,18 @@
 
 var blacklist, whitelist;
 
+updateBlacklist();
+updateWhitelist();
 
-$.getJSON("../blacklist.json", function (data) {
-  blacklist = data;
-});
+// Update blacklist every 5 minutes
+setInterval(function () {
+  updateBlacklist();
+}, 5 * 60 * 1000);
 
-$.getJSON("../whitelist.json", function (data) {
-  whitelist = data;
-});
+// Update whitelist every 24 hours
+setInterval(function () {
+  updateWhitelist();
+}, 24 * 60 * 60 * 1000);
 
 chrome.browserAction.setBadgeText({ text: ' ' });
 chrome.browserAction.setBadgeBackgroundColor({ color: '#969696' });
@@ -21,6 +25,18 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 chrome.tabs.onCreated.addListener(function (tab) {
   checkSafety(tab.id);
 });
+
+function updateBlacklist() {
+  $.getJSON("https://raw.githubusercontent.com/phishfort/phishfort-lists/master/blacklists/domains.json", function (data) {
+    blacklist = data;
+  });
+}
+
+function updateWhitelist() {
+  $.getJSON("https://raw.githubusercontent.com/phishfort/phishfort-lists/master/whitelists/domains.json", function (data) {
+    whitelist = data;
+  });
+}
 
 function checkSafety(tabId) {
   if (typeof tabId === 'undefined') {
@@ -43,12 +59,23 @@ function checkSafety(tabId) {
       chrome.browserAction.setBadgeBackgroundColor({ tabId: tabId, color: '#969696' });
       result = { safety: "Unknown", color: '#969696' }
     }
-    chrome.runtime.sendMessage(result, function(response) {});
+    chrome.runtime.sendMessage(result, function (response) { });
   })
 }
 
-function checkMatch(array, domain) {
-  domain = domain.split('/')[2];
+function checkMatch(array, url) {
+  let domain = (new URL(url).hostname),
+    splitArr = domain.split('.'),
+    arrLen = splitArr.length;
+
+  if (arrLen > 2) {
+    domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
+    // Check for Country Code Top Level Domain (ccTLD) (e.g. ".co.uk")
+    if (((splitArr[arrLen - 2].length == 2) || (splitArr[arrLen - 2].length == 3)) && splitArr[arrLen - 1].length == 2) {
+      domain = splitArr[arrLen - 3] + '.' + domain;
+    }
+  }
+
   let match = false;
   array.forEach(element => {
     if (domain.endsWith(element)) {
@@ -58,5 +85,3 @@ function checkMatch(array, domain) {
   });
   return match;
 }
-
-
--- js/report.js
+++ js/report.js
@@ -7,20 +7,36 @@ let maliciousSite = document.getElementById("maliciousSite");
 reportCurrent.onclick = function (element) {
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     maliciousSite.value = tabs[0].url;
-  }); 
+  });
 };
 
-function recaptcha_callback(data) {
-  $.post("https://us-central1-plugin-recaptcha.cloudfunctions.net/validate-captcha", { malicious: document.getElementById("maliciousSite").value, target: document.getElementById("target").value, comment: document.getElementById("comment").value, captcha: data },
+function recaptchaCallback(data) {
+  var url = document.getElementById("maliciousSite").value;
+  var domain = getDomainFromURL(url);
+
+  $.post("https://us-central1-plugin-recaptcha.cloudfunctions.net/validate-captcha", { url: url, malicious: domain, target: document.getElementById("target").value, comment: document.getElementById("comment").value, captcha: data },
     function (returnedData) {
       window.location.replace("success.html");
     }).fail(function (xhr, status, error) {
-      console.log(xhr, xhr.status, status, error)
-      recaptcha_error();
+      recaptchaError();
     });
 }
 
-function recaptcha_error() {
+function recaptchaError() {
   window.location.replace("error.html");
 };
 
+function getDomainFromURL(url) {
+  let domain = (new URL(url).hostname),
+    splitArr = domain.split('.'),
+    arrLen = splitArr.length;
+
+  if (arrLen > 2) {
+    domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
+    // Check for Country Code Top Level Domain (ccTLD) (e.g. ".co.uk")
+    if (((splitArr[arrLen - 2].length == 2) || (splitArr[arrLen - 2].length == 3)) && splitArr[arrLen - 1].length == 2) {
+      domain = splitArr[arrLen - 3] + '.' + domain;
+    }
+  }
+  return domain;
+}
