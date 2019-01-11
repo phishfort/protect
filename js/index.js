5716157b0830ff05cc9b10a08b63657ec01996ac
kyle <syncikin@gmail.com>
2019-01-11 12:56:42 +0200

    added GA

--- js/index.js
+++ js/index.js
@@ -2,6 +2,16 @@
 
 const background = chrome.extension.getBackgroundPage();
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-124583884-3']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   var status = JSON.stringify(request);
   document.getElementById("statusPhrase").textContent = JSON.parse(status).safety;
