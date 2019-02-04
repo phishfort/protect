39a6a19e0e3e0da448ef4f558c4a16e63b0decb9
kyle <syncikin@gmail.com>
2019-02-04 11:47:14 +0800

    display status bug fix

--- js/background.js
+++ js/background.js
@@ -2,6 +2,14 @@
 
 const browser = window.chrome || window.msBrowser || window.browser;
 
+const SAFE_COLOR = "#0071bc";
+const SAFE_LABEL = "Safe";
+
+const UNKNOWN_COLOR = "#969696";
+const UNKNOWN_LABEL = "Unknown";
+
+const DANGEROUS_COLOR = "#ff0400";
+const DANGEROUS_LABEL = "Dangerous";
 
 const tabs = {};
 var blacklist, whitelist;
@@ -20,7 +28,7 @@ setInterval(function () {
 }, 24 * 60 * 60 * 1000);
 
 browser.browserAction.setBadgeText({ text: ' ' });
-browser.browserAction.setBadgeBackgroundColor({ color: '#969696' });
+browser.browserAction.setBadgeBackgroundColor({ color: UNKNOWN_COLOR });
 
 function updateBlacklist() {
   $.getJSON("https://raw.githubusercontent.com/phishfort/phishfort-lists/master/blacklists/domains.json", function (data) {
@@ -38,22 +46,21 @@ var result = {};
 browser.webRequest.onBeforeRequest.addListener(
   (request) => {
     if (request.tabId >= 0) {
-      let isWarningPage = request.url.startsWith(browser.extension.getURL("../html/warning.html"));
+      let isWarningPage = request.url.startsWith(browser.extension.getURL("html/warning.html"));
       let domain = getDomainFromURL(request.url);
-
       if (domainInArray(domain, whitelist)) {
-        tabs[request.tabId] = { state: "safe" };
+        tabs[request.tabId] = { state: SAFE_LABEL };
       } else if (domainInArray(domain, blacklist) ||
         bypassDomains.includes(domain) ||
         isWarningPage) {
-        tabs[request.tabId] = { state: "dangerous" }
+        tabs[request.tabId] = { state: DANGEROUS_LABEL }
         if (!bypassDomains.includes(domain) && !isWarningPage) {
           return {
             redirectUrl: browser.extension.getURL("../html/warning.html") + "?url=" + request.url
           }
         }
       } else {
-        tabs[request.tabId] = { state: "unknown" };
+        tabs[request.tabId] = { state: UNKNOWN_LABEL };
       }
     }
   }, {
@@ -62,16 +69,52 @@ browser.webRequest.onBeforeRequest.addListener(
 
 browser.runtime.onMessage.addListener((request) => {
   if (request.bypassDomain) {
-    browser.tabs.query({ active: true }, function (tabs) {
-      let tabIndex = tabs.findIndex(tab => tab.url.includes(request.url));
-      let url = tabs[tabIndex].url.split("?url=", 2)[1];
+    browser.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      let url = tabs[0].url.split("?url=", 2)[1];
       let domain = getDomainFromURL(url);
 
       bypassDomains.push(domain);
-      browser.tabs.update(tabs[tabIndex].id, { url: url });
+      browser.tabs.update(tabs[0].id, { url: url });
+    });
+  }
+  if (request.popup) {
+    browser.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      updateIcon(tabs[0].id);
     });
   }
 });
+
+function updateIcon(tabId) {
+  if (tabs[tabId] == null || tabs[tabId].state === UNKNOWN_LABEL) {
+    browser.browserAction.setBadgeBackgroundColor({ tabId: tabId, color: UNKNOWN_COLOR });
+    updatePopup(UNKNOWN_LABEL, UNKNOWN_COLOR);
+  } else if (tabs[tabId].state === SAFE_LABEL) {
+    browser.browserAction.setBadgeBackgroundColor({ tabId: tabId, color: SAFE_COLOR });
+    updatePopup(SAFE_LABEL, SAFE_COLOR);
+  } else if (tabs[tabId].state === DANGEROUS_LABEL) {
+    browser.browserAction.setBadgeBackgroundColor({ tabId: tabId, color: DANGEROUS_COLOR });
+    updatePopup(DANGEROUS_LABEL, DANGEROUS_COLOR);
+  }
+}
+
+function updatePopup(text, color) {
+  var views = browser.extension.getViews({ type: "popup" });
+  if (views.length > 0) {
+    views[0].document.getElementById("statusPhrase").textContent = text;
+    views[0].document.getElementById("statusPhrase").style.color = color;
+    views[0].document.getElementById("statusIndicator").style.color = color;
+  }
+}
+
+function getDomainFromURL(url) {
+  return (new URL(url)).hostname.replace(/^www\./, '');
+}
+
+function domainInArray(currentDomain, arr) {
+  return arr.some(function (domain) {
+    return currentDomain === domain || currentDomain.endsWith('.' + domain);
+  });
+}
 
 browser.tabs.onCreated.addListener(function (tab) {
   updateIcon(tab.id);
@@ -85,22 +128,3 @@ browser.tabs.onUpdated.addListener(function (tabId) {
   updateIcon(tabId);
 });
 
-function updateIcon(tabId) {
-  if (tabs[tabId] == null || tabs[tabId].state === "unknown") {
-    browser.browserAction.setBadgeBackgroundColor({ tabId: tabId, color: '#969696' });
-  } else if (tabs[tabId].state === "safe") {
-    browser.browserAction.setBadgeBackgroundColor({ tabId: tabId, color: '#0071bc' });
-  } else if (tabs[tabId].state === "dangerous") {
-    browser.browserAction.setBadgeBackgroundColor({ tabId: tabId, color: '#ff0400' });
-  }
-}
-
-function getDomainFromURL(url) {
-  return (new URL(url)).hostname.replace(/^www\./,'');
-}
-
-function domainInArray(currentDomain, arr) {
-  return arr.some(function (domain) {
-    return currentDomain === domain || currentDomain.endsWith('.' + domain);
-  });
-}
--- js/index.js
+++ js/index.js
@@ -4,3 +4,5 @@ let reportBtn = document.getElementById("reportButton");
 reportBtn.onclick = function (element) {
   browser.tabs.create({ url: browser.extension.getURL("../html/report.html") });
 };
+
+browser.runtime.sendMessage({popup: true});
