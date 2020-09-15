a4a2b52fbc3b5669df6d89cd67821547f69b6b93
korn101 <jaime2psp@gmail.com>
2020-09-15 12:05:23 +0200

    Added identification of dangerous url as per Issue#21

--- js/warning.js
+++ js/warning.js
@@ -1,9 +1,14 @@
-'use strict';
+"use strict";
 
 const browser = window.msBrowser || window.browser || window.chrome;
 
 let bypassButton = document.getElementById("bypassButton");
+let bypassUrlDisplay = document.getElementById("bypassUrlDisplay");
+let bypassUrl = window.location.search.substr(window.location.search.indexOf("=") + 1);
+if (!!bypassUrl) {
+    bypassUrlDisplay.textContent = bypassUrl;
+}
 
 bypassButton.addEventListener("click", function (event) {
-  browser.runtime.sendMessage({ func: "bypassDomain", url: window.location.href });
+    browser.runtime.sendMessage({ func: "bypassDomain", url: window.location.href });
 });
